1328099842b3cba64a248f3328ea4d625556e8bf
Graham Bewley <grahambewley@gmail.com>
2023-11-10 10:45:13 -0500

    add files first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import rambleman from './ramble-man.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="image-wrapper">
+        <img src={rambleman} className="ramble-man" alt="logo" />
+      </div>
+      <div className="content-wrapper">
+        <p>Hi, I'm <a href="https://www.instagram.com/graham.bewley">Graham</a> 👋</p>
+        <p>The Ramble is a new group bike ride that I'm organizing, kicking off in <strong>Spring of 2024</strong>.</p>
+        <p>We won't be smashing PRs, shred singletrack, or really getting anywhere too fast. Each week will be a bit different, but routes will typically be <strong>15-20 miles</strong> in length, <strong>exploring unique terrain</strong> and <strong>avoiding busy roads</strong> wherever possible.</p>
+        <p>If you're interested in coming along</p>
+      </div>
     </div>
   );
 }
